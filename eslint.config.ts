2af63082236a6387f1e5b5b0d1b1f49127ad0a25
Glener Martins <gleeny.gpm@gmail.com>
2026-09-05 17:42:25 -0300

    iniciando novo projeto

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist", "eslint.config.ts"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: [
+      "js/recommended",
+      tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      pluginReact.configs.flat["jsx-runtime"],
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  eslintConfigPrettier,
+]);
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hello, Vite!</h1>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
